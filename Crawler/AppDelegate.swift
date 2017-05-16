c92e629aea4ac7666087fe06d648f237a672b1ab
nguoimaaicungbietlaai <nguoimaaicungbietlaai@aibietlaaidau.ahihi>
2017-05-16 18:42:06 +0700

    Initial Commit

---
+++ Crawler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Crawler
+//
+//  Created by Son Dang on 5/16/17.
+//  Copyright © 2017 Son Dang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Crawler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Crawler
+//
+//  Created by Son Dang on 5/16/17.
+//  Copyright © 2017 Son Dang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
